f34bff79b59d392b12ebb53238b24a6b9c6487df
James Atwood <jatwood@cs.umass.edu>
2015-01-24 10:37:49 -0500

    Added some interfaces for dealing with text

---
+++ server/interfaces.py
@@ -0,0 +1,59 @@
+__author__ = 'jatwood'
+
+class Lexicon:
+    '''
+    A Lexicon represents everything a user has written.
+
+    The Lexicon manages the storage and processing of text from different sources,
+    and also provides iterators over processed text.
+    '''
+
+    def __init__(self, sources=[]):
+        self.sources = sources
+
+    def __iter__(self):
+        pass
+
+class Source:
+    '''
+    A Source provides an interface to a particular text source while managing storage.
+    '''
+
+    def __init__(self):
+        pass
+
+
+class SourceProcessor:
+    '''
+    A SourceProcessor manages the storage and computation of a text transformation on a particular Source.
+    '''
+
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
+
+
+class Processor:
+    '''
+    A Processor computes a text transformation.
+    '''
+
+    def __init__(self):
+        pass
+
+    def process(self, text):
+        pass
+
+class Pipeline:
+    '''
+    A Pipeline composes Processors into a chain of operations.
+    '''
+
+    def __init__(self,processors):
+        self.processors = processors
+
+
+
+
