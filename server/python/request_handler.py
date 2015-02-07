a98bb5ffb9650fc8019db8ec527012d269c608cc
Emma Strubell <strubell@cs.umass.edu>
2015-02-07 13:22:43 -0500

    debugging?

--- server/python/request_handler.py
+++ server/python/request_handler.py
@@ -1,20 +1,19 @@
-# from urlparse import parse_qs
-# from cgi import escape
-# import mongo_wrapper as mongo
-# import facebook
-# import generate_text_markov as mc
+from urlparse import parse_qs
+from cgi import escape
+import mongo_wrapper as mongo
+import facebook
+import generate_text_markov as mc
 
 
 def application(environ, start_response):
     status = '200 OK'
 
     # parse the http request
-    # request_dict = parse_qs(environ['QUERY_STRING'])
-    # user_name = escape(request_dict.get('name', [''])[0])
-    # access_token = escape(request_dict.get('access_token', [''])[0])
+    request_dict = parse_qs(environ['QUERY_STRING'])
+    user_name = escape(request_dict.get('name', [''])[0])
+    access_token = escape(request_dict.get('access_token', [''])[0])
 
-    # mongo.insert_access_token(user_name, access_token, 'facebook')
-    user_name = "poop"
+    mongo.insert_access_token(user_name, access_token, 'facebook')
     response = 'Success ' + user_name + "\n" #+ sent
 
     response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(response)))]
